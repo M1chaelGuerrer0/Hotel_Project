26d09a991d36031c6fbd2e95eb6be5d6ce247fc5
Emmanuel-S01 <emmanuel.sanchez.250@my.csun.edu>
2025-04-02 13:44:17 -0700

    update

---
+++ Reservation.java
@@ -0,0 +1,34 @@
+public class Reservation {
+    private String checkInDate;
+    private String checkOutDate;
+    private int revId;
+
+
+    public void setCheckInDate(String checkInDate) {
+        this.checkInDate = checkInDate;
+    }
+
+    public String getCheckInDate() {
+        return checkInDate;
+    }
+
+    public void setCheckOutDate(String checkOutDate) {
+        this.checkOutDate = checkOutDate;
+    }
+
+    public String getCheckOutDate() {
+        return checkOutDate;
+    }
+
+    public void setRevId(int revId) {
+        this.revId = revId;
+    }
+
+    public int getRevId() {
+        return revId;
+    }
+
+
+
+
+}
