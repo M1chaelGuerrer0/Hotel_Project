a4d827064ea14cad22b5554b740844cf78b66abe
Michael Guerrero <143291089+M1chaelGuerrer0@users.noreply.github.com>
2025-04-15 14:51:49 -0700

    Add files via upload

--- User.java
+++ User.java
@@ -1,10 +1,17 @@
 public class User {
     private String first;
     private String last;
-
     private String phoneNumber;
     private String email;
     private int userId;
+    private String address1;
+    private String address2;
+    private String city;
+    private String state;
+    private String zipCode;
+    private String country;
+    private int cardId;
+    private String password;
 
 
     public void setFirst(String first){this.first = first;}
@@ -20,12 +27,76 @@ public class User {
     public int getUserId(){return userId;}
 
 
+    public String getPassword() {
+        return password;
+    }
 
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
+    public String getAddress1() {
+        return address1;
+    }
 
+    public void setAddress1(String address1) {
+        this.address1 = address1;
+    }
 
+    public String getAddress2() {
+        return address2;
+    }
 
+    public void setAddress2(String address2) {
+        this.address2 = address2;
+    }
 
+    public String getCity() {
+        return city;
+    }
 
+    public void setCity(String city) {
+        this.city = city;
+    }
 
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public int getCardId() {
+        return cardId;
+    }
+
+    public void setCardId(int cardId) {
+        this.cardId = cardId;
+    }
+
+    public void displayUser() {
+        System.out.println("userId: " + this.userId +"\nfirst name: " + this.first + "\nlast name: " + this.last +
+                "\npassword: "+ this.password + "\nphoneNumber: " + this.phoneNumber + "\nemail: " + this.email +
+                "\naddress1: " + this.address1 + "\naddress2: " + this.address2 + "\ncity: " + this.city +
+                "\ncountry: " + this.country + "\nstate: " + this.state + "\nzipCode: " + this.zipCode +
+                "\ncardId:" + this.cardId
+        );
+    }
 }
