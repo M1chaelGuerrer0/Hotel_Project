6b5b3db16a4145d747c67a96a249ae3c59fbc41b
Ivan Amaya <122948929+iamaya729@users.noreply.github.com>
2025-04-24 16:45:47 -0700

    Separated edit scene controls

--- AccountController.java
+++ AccountController.java
@@ -38,6 +38,9 @@ public class AccountController extends HotelDataBase {
     private Button save;
 
     @FXML
+    private Button initialize;
+
+    @FXML
     private Label emailDisplay;
 
     @FXML
@@ -61,43 +64,6 @@ public class AccountController extends HotelDataBase {
     @FXML
     private Label cityDisplay;
 
-    @FXML
-    private TextField firstName;
-
-    @FXML
-    private TextField lastName;
-
-    @FXML
-    private TextField phoneNum;
-
-    @FXML
-    private TextField userEmail;
-
-    @FXML
-    private TextField userPassword;
-
-    @FXML
-    private TextField confirmPassword;
-
-    @FXML
-    private TextField street1;
-
-    @FXML
-    private TextField street2;
-
-    @FXML
-    private TextField userCity;
-
-    @FXML
-    private TextField zip;
-
-    @FXML
-    private TextField userState;
-
-    @FXML
-    private TextField userCountry;
-
-
     private Parent root;
 
     private Scene scene;
@@ -105,90 +71,16 @@ public class AccountController extends HotelDataBase {
     private Stage stage;
 
 
-    /*
-        Warming prompt indicating that the users passwords do not match
-    */
-    public void mismatch() {
-        Alert alert = new Alert(Alert.AlertType.ERROR);
-        alert.setTitle("Password Mismatch");
-        alert.setHeaderText("Your passwords do not match.");
-        alert.setContentText("Please review your passwords.");
-
-        if(alert.showAndWait().get() == ButtonType.OK) {
-            System.out.println("1");
-        }
-    }
-
-    /*
-        Allows new inputs to replace old email and password, then store that in the database
-
-        @param event listens for when an event fires
-    */
-    public void saveButton(ActionEvent event) throws IOException {
-        String firstname = firstName.getText();
-        System.out.println(firstname);
-        String lastname = lastName.getText();
-        System.out.println(lastname);
-        String email = userEmail.getText();
-        System.out.println(email);
-        String password = userPassword.getText();
-        System.out.println(password);
-        String conPass = confirmPassword.getText();
-        System.out.println(conPass);
-        String phone = phoneNum.getText();
-        System.out.println(phone);
-        String address1 = street1.getText();
-        System.out.println(address1);
-        String address2 = street2.getText();
-        System.out.println(address2);
-        String city = userCity.getText();
-        System.out.println(city);
-        String country = userCountry.getText();
-        System.out.println(country);
-        String state = userState.getText();
-        System.out.println(state);
-        String zipCode = zip.getText();
-        System.out.println(zipCode);
-
-        User guest = new User();
-
-        int match = password.compareTo(conPass);           // compares the two password input to confirm if they match
-                                                           // will return 0, if a mismatch will return an int != 0
-        System.out.println(match);
-
-
-        /*
-            Checks whether match was 0, and if so then the password is saved and the scene is swapped to back to
-            the Login scene, and if not mismatch() is called and the user is prompted to check their entered
-            information
-        */
-        if (match == 0) {
-            // Store all the above info into the database
-            guest.setFirst(firstname);
-            guest.setLast(lastname);
-            guest.setEmail(email);
-            guest.setPassword(password);
-            guest.setPhoneNumber(phone);
-            guest.setAddress1(address1);
-            guest.setAddress2(address2);
-            guest.setCity(city);
-            guest.setCountry(country);
-            guest.setState(state);
-            guest.setZipCode(zipCode);
-            guest.displayUser();                                // display current guest
-
-            addGuest(guest);
-            System.out.println("Your passwords match.");
-
-            root = FXMLLoader.load(getClass().getResource("Account.fxml"));
-            stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-            scene = new Scene(root);
-            stage.setScene(scene);
-            stage.show();
-        } else {
-            System.out.println("Your passwords do not match.");
-            mismatch();
-        }
+    // TODO : Need to retrieve and display account info onto "null" labels
+    public void displayInfo() {
+        emailDisplay.setText("null");
+        passwordDisplay.setText("null");
+        address1Display.setText("null");
+        address2Display.setText("null");
+        zipDisplay.setText("null");
+        stateDisplay.setText("null");
+        countryDisplay.setText("null");
+        cityDisplay.setText("null");
     }
 
     /*
@@ -196,7 +88,7 @@ public class AccountController extends HotelDataBase {
 
         @param event listens for when an event fires
     */
-    public void cancelAccount(ActionEvent event) throws IOException {
+    public void cancelButton(ActionEvent event) throws IOException {
         root = FXMLLoader.load(getClass().getResource("parent.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
@@ -218,17 +110,9 @@ public class AccountController extends HotelDataBase {
     }
 
     /*
-        Switches from the editing Account info scene back to the regular Account scene
-
-        @param event listens for when an event fires
+        Initializes the scene by firing the button automatically to display the user's account information
     */
-    public void cancelEdit(ActionEvent event) throws IOException {
-        root = FXMLLoader.load(getClass().getResource("account.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
-    }
+    public void initialize() { initialize.fire(); }
 
 
 }
