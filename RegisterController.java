2f119787d6d54fe0f775728d549222e45246738e
Ivan Amaya <122948929+iamaya729@users.noreply.github.com>
2025-04-29 16:27:55 -0700

    small change

--- RegisterController.java
+++ RegisterController.java
@@ -115,28 +115,29 @@ public class RegisterController extends HotelDataBase {
     */
     public void registerButton(ActionEvent event) throws IOException {
         String firstname = firstName.getText();
-        System.out.println(firstname);
         String lastname = lastName.getText();
-        System.out.println(lastname);
         String email = userEmail.getText();
-        System.out.println(email);
         String password = userPassword.getText();
-        System.out.println(password);
         String conPass = confirmPassword.getText();
-        System.out.println(conPass);
         String phone = phoneNum.getText();
-        System.out.println(phone);
         String address1 = street1.getText();
-        System.out.println(address1);
         String address2 = street2.getText();
-        System.out.println(address2);
         String city = userCity.getText();
-        System.out.println(city);
         String country = userCountry.getText();
-        System.out.println(country);
         String state = userState.getText();
-        System.out.println(state);
         String zipCode = zip.getText();
+
+        System.out.println(firstname);
+        System.out.println(lastname);
+        System.out.println(email);
+        System.out.println(password);
+        System.out.println(conPass);
+        System.out.println(phone);
+        System.out.println(address1);
+        System.out.println(address2);
+        System.out.println(city);
+        System.out.println(country);
+        System.out.println(state);
         System.out.println(zipCode);
 
         int match = password.compareTo(conPass);           // compares the two password input to confirm if they match
